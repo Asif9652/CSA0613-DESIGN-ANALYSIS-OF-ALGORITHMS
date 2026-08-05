7f37c72d14691db7817a1a94add0044979150bef
Asif9652 <rayadurgamasif@gmail.com>
2026-08-05 21:37:11 +0530

    add co3

--- CO2/CO2_AT3/Main.java
+++ CO2/CO2_AT3/Main.java
@@ -1,84 +1,58 @@
-import java.util.ArrayList;
+public class Main {
 
-class Point {
-    int x, y;
+    // Optimized Bubble Sort (Ascending Order)
+    public static void bubbleSort(int[] arr) {
 
-    Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-}
+        int n = arr.length;
 
-public class ConvexHull {
+        // Traverse through all array elements
+        for (int i = 0; i < n - 1; i++) {
 
-    static int orientation(Point p, Point q, Point r) {
-        int val = (q.y - p.y) * (r.x - q.x)
-                - (q.x - p.x) * (r.y - q.y);
+            // Flag to check if swapping happened
+            boolean swapped = false;
 
-        if (val == 0)
-            return 0;
+            // Last i elements are already sorted
+            for (int j = 0; j < n - i - 1; j++) {
 
-        return (val > 0) ? 1 : 2;
-    }
+                // Compare adjacent elements
+                if (arr[j] > arr[j + 1]) {
 
-    static void bruteForceHull(Point points[], int n) {
+                    // Swap if left element is greater
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
 
-        ArrayList<Point> hull = new ArrayList<>();
-
-        if (n < 3) {
-            System.out.println("Convex Hull not possible");
-            return;
-        }
-
-        for (int i = 0; i < n; i++) {
-
-            for (int j = i + 1; j < n; j++) {
-
-                int pos = 0, neg = 0;
-
-                for (int k = 0; k < n; k++) {
-
-                    if (k == i || k == j)
-                        continue;
-
-                    int val = (points[j].x - points[i].x) * (points[k].y - points[i].y)
-                            - (points[j].y - points[i].y) * (points[k].x - points[i].x);
-
-                    if (val > 0)
-                        pos++;
-                    else if (val < 0)
-                        neg++;
-                }
-
-                if (pos == 0 || neg == 0) {
-
-                    if (!hull.contains(points[i]))
-                        hull.add(points[i]);
-
-                    if (!hull.contains(points[j]))
-                        hull.add(points[j]);
+                    swapped = true;
                 }
             }
+
+            // Stop if array is already sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    // Print array
+    public static void printArray(int[] arr) {
+
+        for (int num : arr) {
+            System.out.print(num + " ");
         }
 
-        System.out.println("Convex Hull Points:");
-
-        for (Point p : hull)
-            System.out.println("(" + p.x + "," + p.y + ")");
+        System.out.println();
     }
 
     public static void main(String[] args) {
 
-        Point points[] = {
-                new Point(0, 3),
-                new Point(2, 2),
-                new Point(1, 1),
-                new Point(2, 1),
-                new Point(3, 0),
-                new Point(0, 0),
-                new Point(3, 3)
-        };
+        int[] arr = {7, 3, 9, 2, 5, 1};
 
-        bruteForceHull(points, points.length);
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        bubbleSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
     }
 }
---
+++ CO3_AT2/Question_30/Source_Code/NetworkPacketAnalysis.java
@@ -0,0 +1,127 @@
+import java.util.*;
+
+public class NetworkPacketAnalysis {
+
+    // Merge Sort
+    public static void mergeSort(int[] arr, int left, int right) {
+
+        if (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            mergeSort(arr, left, mid);
+
+            mergeSort(arr, mid + 1, right);
+
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int left, int mid, int right) {
+
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[left + i];
+
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[mid + 1 + j];
+
+        int i = 0;
+        int j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
+    }
+
+    // Binary Search
+
+    public static int binarySearch(int arr[], int target) {
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] < target)
+                left = mid + 1;
+
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of packets: ");
+        int n = sc.nextInt();
+
+        int packets[] = new int[n];
+
+        System.out.println("Enter packet IDs:");
+
+        for (int i = 0; i < n; i++) {
+            packets[i] = sc.nextInt();
+        }
+
+        mergeSort(packets, 0, n - 1);
+
+        System.out.println("\nSorted Packet IDs:");
+
+        for (int x : packets)
+            System.out.print(x + " ");
+
+        System.out.println();
+
+        System.out.print("\nEnter Packet ID to Search: ");
+
+        int target = sc.nextInt();
+
+        int result = binarySearch(packets, target);
+
+        if (result != -1)
+            System.out.println("Packet Found at Index : " + result);
+
+        else
+            System.out.println("Packet Not Found.");
+
+        sc.close();
+    }
+}
